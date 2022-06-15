25c27220ae59bcd8747693cad64cd33e1fd4fb56
korayuymaz <korayuymaz@gmail.com>
2022-06-15 07:11:35 +0300

    Final edition

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,15 +1,22 @@
 const githubReducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'GET_USERS':
       return {
         ...state,
         users: action.payload,
-        loading: false
+        loading: false,
+      }
+    case 'GET_USER_AND_REPOS':
+      return {
+        ...state,
+        user: action.payload.user,
+        repos: action.payload.repos,
+        loading: false,
       }
     case 'SET_LOADING':
       return {
         ...state,
-        loading: true
+        loading: true,
       }
     case 'CLEAR_USERS':
       return {
